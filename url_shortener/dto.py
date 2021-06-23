bb4cb8172c75ecc41c60b7edd7dd08e17341c732
loay elhefni <lalhefny57@gmail.com>
2021-06-23 12:32:36 +0300

    fix bugs user creation works

--- url_shortener/dto.py
+++ url_shortener/dto.py
@@ -1,9 +1,10 @@
 """
 dto stands for Data Transfer Object
 """
-from typing import NamedTuple
+from typing import NamedTuple, Dict
 
 
 class User(NamedTuple):
     """Sample User DTO, which is used to represent authenticated user in out system"""
     email: str
+    password: str
