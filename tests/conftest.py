aaa013774f0b3aa9af66a14882bfe8f3c78e4561
loay elhefni <lalhefny57@gmail.com>
2021-06-23 12:23:10 +0300

    Add basic request testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+  
 import os
 from pyramid.paster import get_appsettings
 from pyramid.scripting import prepare
@@ -9,9 +10,12 @@ from url_shortener.main import make_app
 from url_shortener.config import Config
 
 
+
 @pytest.fixture(scope='session')
 def app():
-    config = Config(base_url='http://127.0.0.1:6543', port=6543)
+    config = Config(base_url='http://127.0.0.1:6543',
+                    port=6543,
+                    user_db='test_users.json')
     return make_app(config)
 
 
@@ -20,31 +24,13 @@ def testapp(app):
     testapp = webtest.TestApp(app)
     return testapp
 
-
+    
 @pytest.fixture
-def app_request(app):
-    """
-    A real request.
-    This request is almost identical to a real request but it has some
-    drawbacks in tests as it's harder to mock data and is heavier.
-    """
-    with prepare(registry=app.registry) as env:
-        request = env['request']
-        request.host = 'example.com'
-        yield request
+def glob():
+    email1 = "test@tests.test"
+    passw1 = "test-password"
+    url1 = "https://google.com"
+    url_short: str
+    token1: str
 
-
-@pytest.fixture
-def dummy_request():
-    """
-    A lightweight dummy request.
-    This request is ultra-lightweight and should be used only when the request
-    itself is not a large focus in the call-stack.  It is much easier to mock
-    and control side-effects using this object, however:
-    - It does not have request extensions applied.
-    - Threadlocals are not properly pushed.
-    """
-    request = DummyRequest()
-    request.host = 'example.com'
-
-    return request
+    return locals()
